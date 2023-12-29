e408f969266f4bc7293298cf4e22ef26a17f0c5c
chenchien-ming <alvin@js-adways.com.tw>
2023-12-29 15:14:55 +0800

    edit support or condition

--- src/ScopeFilterTrait.php
+++ src/ScopeFilterTrait.php
@@ -4,6 +4,7 @@ namespace Jsadways\ScopeFilter;
 
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
+use mysql_xdevapi\Collection;
 
 trait ScopeFilterTrait
 {
@@ -27,20 +28,81 @@ trait ScopeFilterTrait
                 array_pop($splitKeys);
                 $field = implode('_', $splitKeys);
 
+                if($rule === 'or'){
+                    $query->where(function($q){
+                        $q->orWhere('');
+                        $q->orWhere('');
+                        $q->orWhere('');
+                        $q->orWhere('');
+                    });
+                    $query = $query->where(function($sub_query)use($v){
+                        foreach ($v as $sub_k=>$sub_v){
+                            $splitKeys = explode('_', $sub_k);
+                            $sub_rule = end($splitKeys);
+                            array_pop($splitKeys);
+                            $sub_field = implode('_', $splitKeys);
+                            $sub_query = self::conditionFilter($sub_query,['field'=>$sub_field,'rule'=>$sub_rule,'value'=>$sub_v],'or');
+                        }
+                    });
+                }else{
+                    $query = self::conditionFilter($query,['field'=>$field,'rule'=>$rule,'value'=>$v],'and');
+                }
+            }
+
+            return $query;
+        } catch (Exception $e) {
+            throw new Exception("scopeFilter - {$e->getMessage()}");
+        }
+    }
+
+    /**
+     * condition filter
+     *
+     * 查詢調整過濾 scoper
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder
+     * @param array[
+     *     'name_k' => ['hello world']' ['{field}_{rule}' => ['value']]
+     * ]
+     */
+    protected function conditionFilter(Builder $query, array $filter, string $type): Builder|Exception
+    {
+        try {
+            $field = $filter['field'];
+            $rule = $filter['rule'];
+            $value = $filter['value'];
+
+            if($type === 'or'){
                 $query = match ($rule) {
-                    'k' => $query->where($field, 'like', "%{$v}%"),
-                    'ipp' => $query->where($field, 'like', "%{$v}"),
-                    'ie' => $query->where($field, 'like', "%{$v}"),
-                    'iel' => $query->where($field, 'ilike', "%{$v}"),
-                    'in' => (is_array($v)) ? $query->whereIn($field, $v) : $query,
-                    'nin' => (is_array($v)) ? $query->whereNotIn($field, $v) : $query,
-                    'ge' => $query->where($field, '>=', $v),
-                    'gt' => $query->where($field, '>', $v),
-                    'ne' => $query->where($field, '!=', $v),
-                    'eq' => $query->where($field, '=', $v),
-                    'lt' => $query->where($field, '<', $v),
-                    'le' => $query->where($field, '<=', $v),
-                    'dr' => (is_array($v)) ? $query->whereBetween($field, [$v[0], $v[1]]) : $query
+                    'k' => $query->orWhere($field, 'like', "%{$value}%"),
+                    'ipp' => $query->orWhere($field, 'like', "%{$value}"),
+                    'ie' => $query->orWhere($field, 'like', "%{$value}"),
+                    'iel' => $query->orWhere($field, 'ilike', "%{$value}"),
+                    'in' => (is_array($value)) ? $query->orWhereIn($field, $value) : $query,
+                    'nin' => (is_array($value)) ? $query->orWhereNotIn($field, $value) : $query,
+                    'ge' => $query->orWhere($field, '>=', $value),
+                    'gt' => $query->orWhere($field, '>', $value),
+                    'ne' => $query->orWhere($field, '!=', $value),
+                    'eq' => $query->orWhere($field, '=', $value),
+                    'lt' => $query->orWhere($field, '<', $value),
+                    'le' => $query->orWhere($field, '<=', $value),
+                    'dr' => (is_array($value)) ? $query->orWhereBetween($field, [$value[0], $value[1]]) : $query
+                };
+            }else{
+                $query = match ($rule) {
+                    'k' => $query->where($field, 'like', "%{$value}%"),
+                    'ipp' => $query->where($field, 'like', "%{$value}"),
+                    'ie' => $query->where($field, 'like', "%{$value}"),
+                    'iel' => $query->where($field, 'ilike', "%{$value}"),
+                    'in' => (is_array($value)) ? $query->whereIn($field, $value) : $query,
+                    'nin' => (is_array($value)) ? $query->whereNotIn($field, $value) : $query,
+                    'ge' => $query->where($field, '>=', $value),
+                    'gt' => $query->where($field, '>', $value),
+                    'ne' => $query->where($field, '!=', $value),
+                    'eq' => $query->where($field, '=', $value),
+                    'lt' => $query->where($field, '<', $value),
+                    'le' => $query->where($field, '<=', $value),
+                    'dr' => (is_array($value)) ? $query->whereBetween($field, [$value[0], $value[1]]) : $query
                 };
             }
 
