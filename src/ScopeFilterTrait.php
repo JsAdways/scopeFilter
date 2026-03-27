6be07a0f85c551243f5113d38c5f513a026ee364
chenchien-ming <alvin@js-adways.com.tw>
2026-03-27 08:41:50 +0800

    fix: restore emptyValidator

--- src/ScopeFilterTrait.php
+++ src/ScopeFilterTrait.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Collection;
 use Jsadways\ScopeFilter\Services\Filter\FilterGetRelationModelDto;
 use Jsadways\ScopeFilter\Services\Validation\ValidateDeriveMethod;
+use Jsadways\ScopeFilter\Services\Validation\ValidateEmpty;
 use ReflectionClass;
 use ReflectionMethod;
 use Jsadways\ScopeFilter\Services\Validation\Validation;
@@ -35,7 +36,7 @@ trait ScopeFilterTrait
     private Builder $query;
     private Validation $keyValidator;
     private Validation $relationValidator;
-    //private Validation $emptyValidator;
+    private Validation $emptyValidator;
     private Validation $columnValidator;
     private Validation $deriveMethodValidator;
 
@@ -125,7 +126,7 @@ trait ScopeFilterTrait
      */
     protected function _validatorInit(): void
     {
-        //$this->emptyValidator = new Validation(new ValidateEmpty());//filter empty collections
+        $this->emptyValidator = new Validation(new ValidateEmpty());//filter empty collections
         $this->keyValidator = new Validation(new ValidateKey());//filter key validator
         $this->relationValidator = new Validation(new ValidateRelation($this));
         $this->columnValidator = new Validation(new ValidateColumn($this));
@@ -204,8 +205,7 @@ trait ScopeFilterTrait
     protected function _fitColumn(array $conditionArray,string $logic): void
     {
         //remove empty conditions
-        //$nonEmptyConditions = $this->emptyValidator->extract(collect($conditionArray));
-        $nonEmptyConditions = collect($conditionArray);
+        $nonEmptyConditions = $this->emptyValidator->extract(collect($conditionArray));
         $filterService = new FilterService();
         //format filed data
         $formattedField = $filterService->format(new FilterFormatDto($nonEmptyConditions));
@@ -298,8 +298,7 @@ trait ScopeFilterTrait
 
         $validRelation->map(function ($conditions, $relationName)use($whereHas,$logic,$query){
             //remove empty conditions
-            //$nonEmptyConditions = $this->emptyValidator->extract(collect($conditions));
-            $nonEmptyConditions = collect($conditions);
+            $nonEmptyConditions = $this->emptyValidator->extract(collect($conditions));
 
             if ($nonEmptyConditions->count() !== 0) {
 
